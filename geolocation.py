205191eb72a7e739883a7cdcb7f98f65f3b847b5
LatvianProgrammer <kalvans.rolands@gmail.com>
2019-01-13 01:47:40 +0200

    prettied up the geolocation module

--- geolocation.py
+++ geolocation.py
@@ -3,13 +3,15 @@ from typing import NamedTuple
 
 
 def distance_between_points(p1, p2):
+    """Returns distance between two (lat,lng) pairs in meters"""
     lon1, lat1 = p1
     lon2, lat2 = p2
     r = 6378.137
     d_lat = lat2 * math.pi / 180 - lat1 * math.pi / 180
     d_lon = lon2 * math.pi / 180 - lon1 * math.pi / 180
-    a = math.sin(d_lat / 2) * math.sin(d_lat / 2) + \
-        math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * math.sin(d_lon / 2) * math.sin(d_lon / 2)
+    a = (math.sin(d_lat / 2) * math.sin(d_lat / 2) +
+         (math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) *
+          math.sin(d_lon / 2) * math.sin(d_lon / 2)))
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     d = r * c
     return d * 1000
