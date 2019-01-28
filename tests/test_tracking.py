7d96c268fbc1185125e58b1ca6a1939513c5d365
LatvianProgrammer <kalvans.rolands@gmail.com>
2019-01-28 17:29:18 +0200

    will be making changes to tracker most likely, want to expand tests for it

--- tests/test_tracking.py
+++ tests/test_tracking.py
@@ -1,6 +1,6 @@
 from minibus_routes import RouteID
 from minibus_tracker import MinibusTracker
-from nose.tools import assert_greater
+from nose.tools import assert_equal
 
 
 def test_refresh():
@@ -9,4 +9,8 @@ def test_refresh():
     for i in range(250):
         minibus_tracker.refresh_minibuses()
 
-    assert_greater(len(minibus_tracker.tracked_minibuses), 0)
+    assert_equal(len(minibus_tracker.tracked_minibuses), 1)
+
+    minibus = minibus_tracker.tracked_minibuses.popitem()[1]
+
+    assert_equal('Katlakalna iela', minibus.stop.name)
