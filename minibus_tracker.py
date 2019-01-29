f6a25f22701635c9f874be736acf5fe489697e73
LatvianProgrammer <kalvans.rolands@gmail.com>
2019-01-29 18:27:15 +0200

    trying to improve usefulness of logging

--- minibus_tracker.py
+++ minibus_tracker.py
@@ -30,8 +30,12 @@ class MinibusTracker(object):
         for car_id, minibus in non_tracked_buses.items():
             if car_id in self.tracked_minibuses or self.at_first_stop(minibus=minibus):
                 if self.at_last_stop(minibus=minibus):
+                    logger.debug('{} drove to last stop'.format(car_id))
                     del self.tracked_minibuses[car_id]
                     continue
+
+                if car_id not in self.tracked_minibuses:
+                    logger.debug('{} tracking started'.format(car_id))
 
                 # fixme: these could probably just be calculated when needed...?
                 minibus.stop_index, minibus.stop = closest_stop(minibus=minibus, stops=self.route_data.stops)
@@ -45,6 +49,7 @@ class MinibusTracker(object):
         for car_id in list(self.tracked_minibuses.keys()):
             if car_id not in non_tracked_buses.keys():
                 times_missing = self.lost_minibuses[car_id] = self.lost_minibuses[car_id] + 1
+                logger.debug('{} is missing for {}'.format(car_id, times_missing))
                 if times_missing > 5:
                     del self.tracked_minibuses[car_id]
                     del self.lost_minibuses[car_id]
